cac5f71f4836e0195b57244d50ac23c5175de737
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 14:09:24 +0200

    print elements of list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t x;
+
+	for (x = 0; h != NULL; x++)
+	{
+		printf("%d\n", h->n)
+		h = h->next;
+	}
+	return (x);
+}
