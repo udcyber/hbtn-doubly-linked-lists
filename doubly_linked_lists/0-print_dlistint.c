a90e1d5d021fa33d6a121c1aaeb3734a48f3689b
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 14:11:09 +0200

    print elements of list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,13 +5,13 @@
  * @h: head
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x;
 
 	for (x = 0; h != NULL; x++)
 	{
-		printf("%d\n", h->n)
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (x);
