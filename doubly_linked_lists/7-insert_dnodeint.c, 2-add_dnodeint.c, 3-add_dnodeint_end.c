d4a8740ef3ec416355ff56e63adbbdaaf2ab98f2
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 15:24:53 +0200

    insert node

--- doubly_linked_lists/7-insert_dnodeint.c, 2-add_dnodeint.c, 3-add_dnodeint_end.c
+++ doubly_linked_lists/7-insert_dnodeint.c, 2-add_dnodeint.c, 3-add_dnodeint_end.c
@@ -15,34 +15,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int x;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
 
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
+
 	old_n = *h;
 
 	for (x = 0; old_n != NULL && x < idx; x++)
-	{
 		old_n = old_n->next;
-	}
 
 	if (old_n == NULL && x == idx)
-	{
 		return (add_dnodeint_end(h, n));
-	}
 
 	else if (old_n != NULL)
 	{
 		new_n = malloc(sizeof(dlistint_t));
 
 		if (new_n == NULL)
-		{
 			return (NULL);
-		}
+
 		new_n->n = n;
 		old_n->prev->next = new_n;
 		new_n->prev = old_n->prev;
