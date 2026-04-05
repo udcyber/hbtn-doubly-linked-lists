bfb80a7f4b50f076c94727897b9bd2952befa5d8
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 14:31:52 +0200

    return nth node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,6 +4,7 @@
  * get_dnodeint_at_index - return the nth node of a dlistint_t linked list
  * @head: head
  * @index: index
+ * Return: head
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
