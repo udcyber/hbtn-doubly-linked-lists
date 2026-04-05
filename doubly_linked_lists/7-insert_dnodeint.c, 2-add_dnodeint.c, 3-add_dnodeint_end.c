e49e8d0dc12a2365214d5f49e4de845443e71188
udcyber <udewaleyne.pro@gmail.com>
2026-04-05 14:34:54 +0200

    insert new node at place of choosing

---
+++ doubly_linked_lists/7-insert_dnodeint.c, 2-add_dnodeint.c, 3-add_dnodeint_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node at a given position
+ * @h: head
+ * @idx: index
+ * @n: integer
+ * Return: new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_n; *old_n;
+	unsigned int x;
+
+	if (h == NULL)
+	{
+		retrn (NULL);
+	}
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	old_n = *h;
+
+	for (x = 0; old_n != NULL && x < idx; x++)
+	{
+		old_n = old_n->next;
+	}
+
+	if (old_n == NULL && x == idx)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	else if (old_n != NULL)
+	{
+		new = malloc(sizeof(dlistint_t));
+
+		if (new_n == NULL)
+		{
+			return (NULL);
+		}
+		new_n->n = n;
+		old_n->prev->next = new_n;
+		new_n->prev = old_n->prev;
+		old_n->prev = new_n;
+		new_n->next = old_n;
+
+		return (new_n);
+	}
+
+	return (NULL);
+}
